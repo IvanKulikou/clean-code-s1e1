0f6d10fee53227b9d62896e4cf1de175ca25b2ab
Ivan Kulikou <valfson@gmail.com>
2024-01-08 18:18:03 +0100

    refactor: finish correecting CSS classes with JS to remain functional according to rule 3.4 begiineer guide

---
+++ .history/app_20240108180052.js
@@ -0,0 +1,114 @@
+
+
+
+
+var taskInput = document.getElementById("new-task");
+var addButton = document.getElementsByTagName("button")[0];
+console.log(addButton);
+var incompleteTaskHolder = document.getElementById("incomplete-tasks"); // <-- Corrected ID
+var completedTasksHolder = document.getElementById("completed-tasks");
+
+var createNewTaskElement = function (taskString) {
+    var listItem = document.createElement("li");
+    listItem.className = "custom-li";
+    var checkBox = document.createElement("input");
+    checkBox.className = "custom-checkbox";
+    var label = document.createElement("label");
+    label.className = "task-list__task";
+    var editInput = document.createElement("input");
+    editInput.type = "text";
+    editInput.className = "task-list__task-input";
+    var editButton = document.createElement("button");
+    var deleteButton = document.createElement("button");
+    var deleteButton = document.createElement("button");
+
+    var deleteButtonImg = document.createElement("img");
+    var deleteButtonImg = document.createElement("img");
+    deleteButtonImg.classList.add("rotate-img");
+    label.innerText = taskString;
+    // label.className = 'task';
+
+    checkBox.type = "checkbox";
+    editInput.type = "text";
+    editInput.className = "custom-input task-list__task-input";
+
+    editButton.innerText = "Edit";
+    editButton.className = "edit btn";
+
+    deleteButton.className = "delete btn";
+    deleteButtonImg.src = './remove.svg';
+    deleteButton.appendChild(deleteButtonImg);
+
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
+    return listItem;
+};
+
+var addTask = function () {
+    if (!taskInput.value) return;
+    var listItem = createNewTaskElement(taskInput.value);
+    incompleteTaskHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+    taskInput.value = "";
+}
+
+var editTask = function () {
+    var listItem = this.parentNode;
+    var editInput = listItem.querySelector('.task-list__task-input');
+    var label = listItem.querySelector("label");
+    var editBtn = listItem.querySelector(".edit");
+    console.log(editBtn);
+
+    var containsClass = listItem.classList.contains("task-list__edit-mode");
+    if (containsClass) {
+        label.innerText = editInput.value;
+        editBtn.innerText = "Edit";
+    } else {
+        editInput.value = label.innerText;
+        editBtn.innerText = "Save";
+    }
+
+    listItem.classList.toggle("task-list__edit-mode");
+};
+
+var deleteTask = function () {
+    var listItem = this.parentNode;
+    var ul = listItem.parentNode;
+    ul.removeChild(listItem);
+}
+
+var taskCompleted = function () {
+    var listItem = this.parentNode;
+    completedTasksHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskIncomplete);
+}
+
+var taskIncomplete = function () {
+    var listItem = this.parentNode;
+    incompleteTaskHolder.appendChild(listItem);
+    bindTaskEvents(listItem, taskCompleted);
+}
+
+var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
+    var checkBox = taskListItem.querySelector("input[type=checkbox]");
+    var editButton = taskListItem.querySelector(".edit");
+    var deleteButton = taskListItem.querySelector(".delete");
+
+    editButton.onclick = editTask;
+    deleteButton.onclick = deleteTask;
+    checkBox.onchange = checkBoxEventHandler;
+}
+
+for (var i = 0; i < incompleteTaskHolder.children.length; i++) {
+    bindTaskEvents(incompleteTaskHolder.children[i], taskCompleted);
+}
+
+for (var i = 0; i < completedTasksHolder.children.length; i++) {
+    bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
+}
+
+addButton.onclick = addTask;
+addButton.addEventListener("click", addTask);
